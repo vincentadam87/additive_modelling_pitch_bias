697965cbea00028e94125e5a68ced9a43cfd85a4
Dell - Linx <itay.lieder@mail.huji.ac.il>
2016-02-02 20:16:09 +0200

    distribution test simulations

--- stimulus_generation/generate_stimuli.py
+++ stimulus_generation/generate_stimuli.py
@@ -63,7 +63,7 @@ def sample_s1_s2_bimodal(samples,franges,dfrange):
             s1[ii],s2[ii] = s2[ii],s1[ii]
     return s1,s2
 
-def sample_s1_s2_threshold(samples,frange,dfrange,threshold,condition):
+def sample_s1_s2_threshold(samples,frange,dfrange,threshold):
     '''
     Sampling frequency pairs, first one according to uniform
     Second one conditional on first setting a difficulty sampled from  U[-df,df]
@@ -74,80 +74,84 @@ def sample_s1_s2_threshold(samples,frange,dfrange,threshold,condition):
     :return: s1, s2
     '''
     dfrange = np.log(dfrange)
-    s1 = np.random.rand(samples) * (np.log(frange[1]) - np.log(frange[0])) + np.log(frange[0])
-    s2 = np.empty(samples)
 
-    s2[0] = s1[0] + (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-    if np.random.rand()>0.5:
-        s2[0] = s1[0] - (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-    if np.random.rand()>0.5:
-        s1[0],s2[0] = s2[0],s1[0]
+    for inner in (-1, 1):
+        s1 = np.random.rand(samples) * (np.log(frange[1]) - np.log(frange[0])) + np.log(frange[0])
+        s2 = np.empty(samples)
 
-    for ii in range(1,samples):
-        dist = s1[ii] - 0.5*(s1[ii-1]+s2[ii-1])
-        direction = np.sign(dist)
-
-        if condition == 'inner_minus':
-            if (np.abs(dist) < threshold):
-                s2[ii] = s1[ii] + direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-            else:
-                s2[ii] = s1[ii] - direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-        if condition == 'inner_plus':
-            if (np.abs(dist) < threshold):
-                s2[ii] = s1[ii] - direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-            else:
-                s2[ii] = s1[ii] + direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-    return s1,s2
-
-def sample_s1_s2_markov(samples, frange, dfrange, mrange_close, mrange_far, condition):
-    '''
-    Sampling frequency pairs, first one according to uniform
-    Second one conditional on first setting a difficulty sampled from  U[-df,df]
-    with resampling if out of range bounds
-    :param samples: number of trials
-    :param frange: frequency range (list)
-    :param dfrange: difficulty range, in Hz
-    :return: s1, s2
-    '''
-
-    if condition == 'inner_plus':
-        inner = -1
-    elif condition == 'inner_minus':
-        inner = 1
-
-    dfrange = np.log(dfrange)
-    s1 = np.empty(samples)
-    s2 = np.empty(samples)
-
-    s1[0] = np.random.rand() * (np.log(frange[1]) - np.log(frange[0])) + np.log(frange[0])
-    s2[0] = s1[0] + (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-    if np.random.rand()>0.5:
-        s2[0] = s1[0] - (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
-    if np.random.rand()>0.5:
-        s1[0],s2[0] = s2[0],s1[0]
-
-    for ii in range(1,samples):
-
+        s2[0] = s1[0] + (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
         if np.random.rand()>0.5:
-            # sample close
-            bias = 1
-            add_sub = np.random.randint(2)*2-1
-            s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_close[1] - mrange_close[0])+ mrange_close[0])
-            while (s1[ii] < np.log(frange[0])) or (s1[ii] > np.log(frange[1])):
+            s2[0] = s1[0] - (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+        if np.random.rand()>0.5:
+            s1[0],s2[0] = s2[0],s1[0]
+
+        for ii in range(1,samples):
+            dist = s1[ii] - 0.5*(s1[ii-1]+s2[ii-1])
+            direction = np.sign(dist)
+
+            if (np.abs(dist) < threshold):
+                s2[ii] = s1[ii] + inner*direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+            else:
+                s2[ii] = s1[ii] - inner*direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+
+        if inner == -1:
+            s1_pm = s1
+            s2_pm = s2
+        elif inner == 1:
+            s1_mp = s1
+            s2_mp = s2
+
+    return s1_pm, s2_pm, s1_mp, s2_mp
+
+def sample_s1_s2_markov(samples, frange, dfrange, mrange_close, mrange_far):
+    '''
+    Sampling frequency pairs, first one according to uniform
+    Second one conditional on first setting a difficulty sampled from  U[-df,df]
+    with resampling if out of range bounds
+    :param samples: number of trials
+    :param frange: frequency range (list)
+    :param dfrange: difficulty range, in Hz
+    :return: s1, s2
+    '''
+    dfrange = np.log(dfrange)
+    for inner in (-1, 1):
+        s1 = np.empty(samples)
+        s2 = np.empty(samples)
+
+        s1[0] = np.random.rand() * (np.log(frange[1]) - np.log(frange[0])) + np.log(frange[0])
+        s2[0] = s1[0] + (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+        if np.random.rand()>0.5:
+            s2[0] = s1[0] - (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+        if np.random.rand()>0.5:
+            s1[0],s2[0] = s2[0],s1[0]
+
+        for ii in range(1,samples):
+
+            if np.random.rand()>0.5:
+                # sample close
+                bias = 1
                 add_sub = np.random.randint(2)*2-1
-                s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_close[1] - mrange_close[0])+mrange_close[0])
-
-
-        else:
-            # sample far
-            bias = -1
-            add_sub = np.random.randint(2)*2-1
-            s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_far[1] - mrange_far[0]) + mrange_far[0])
-            while (s1[ii] < np.log(frange[0])) or (s1[ii] > np.log(frange[1])):
+                s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_close[1] - mrange_close[0])+ mrange_close[0])
+                while (s1[ii] < np.log(frange[0])) or (s1[ii] > np.log(frange[1])):
+                    add_sub = np.random.randint(2)*2-1
+                    s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_close[1] - mrange_close[0])+mrange_close[0])
+            else:
+                # sample far
+                bias = -1
                 add_sub = np.random.randint(2)*2-1
                 s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_far[1] - mrange_far[0]) + mrange_far[0])
+                while (s1[ii] < np.log(frange[0])) or (s1[ii] > np.log(frange[1])):
+                    add_sub = np.random.randint(2)*2-1
+                    s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_far[1] - mrange_far[0]) + mrange_far[0])
+            dist = s1[ii] - 0.5*(s1[ii-1]+s2[ii-1])
+            s2[ii] = s1[ii] + inner*bias*np.sign(dist)*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
 
-        dist = s1[ii] - 0.5*(s1[ii-1]+s2[ii-1])
-        s2[ii] = s1[ii] + inner*bias*np.sign(dist)*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+        if inner == -1:
+            s1_pm = s1
+            s2_pm = s2
+        elif inner == 1:
+            s1_mp = s1
+            s2_mp = s2
 
-    return s1,s2
+
+    return s1_pm, s2_pm, s1_mp, s2_mp
