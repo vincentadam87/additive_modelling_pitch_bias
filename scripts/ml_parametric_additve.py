0ecac51c7635dab4c2030b683dd5ba21814f296e
vincentadam87 <vincent.adam87@gmail.com>
2016-02-02 20:16:52 +0200

    minor updates

--- scripts/ml_parametric_additve.py
+++ scripts/ml_parametric_additve.py
@@ -1,91 +1,70 @@
-from scipy.optimize import minimize
+'''
+This script performs maximum likelihood estimates of the
+'''
+
+
 from model_simulations.descriptive_model import *
 from utils import *
 from matplotlib import pyplot as plt
 import matplotlib.patheffects as path_effects
 # ---------------------- Additive Linear Exp ---------------------
 
-bias = lambda w,d : lambda x : w*x*exp(-abs(x)/d)
-lik = lambda l : lambda x : l/2+(1-l)*phi(x)
 
-
-# Parameters:  lambda, sigma, additive pairs
-
-
-def flat_prm(p_add,p_lin,p_lik):
-    return np.concatenate([np.array(p_add).flatten(),[p_lin],[p_lik]])
-
-def unflat_param(w):
-    t = len(w)/2-1 # number of lags
-    p_add = [[w[2*l],w[2*l+1]] for l in range(t)]
-    p_lin = w[-2]
-    p_lik = w[-1]
-    return p_add,p_lin,p_lik
-
-def bern(w,x):
-    p_add,p_lin,p_lik = unflat_param(w)
-    b = LinExpAdditiveModel(p_add,p_lin,p_lik).bern(x)
-    eps = 1e-5
-    b[b>1-eps]=1-eps
-    b[b<eps]=eps
-    return b
-
-def llh(w,x,y):
-    b = bern(w,x)
-    return np.sum( (y*np.log(b) + (1-y)*np.log(1-b))  )
 
 
 if __name__ == '__main__':
 
 
+    # declare model parameters for simulations
+
+
     n =5000
     d = 2
 
-    # declare model
-
-    peaks = [0.1,0.5]
-    maxs = np.ones((d,))*1
-    prm_add = [[m*1./peak*np.exp(1), 1./peak] for peak, m in zip(peaks,maxs)]
-
-    #prm_add = [[0.5,1],[0.6,2],[0.5,3]] # parameters of additive functions
     prm_lin = 1./0.02 # linear parameter
-    prm_lik = .2 # parameter of the likelihood
+    prm_lik = .01 # parameter of the likelihood
 
-    w = flat_prm(prm_add,prm_lin,prm_lik)
-    mod = LinExpAdditiveModel(prm_add,prm_lin,prm_lik)
+    type = 'LinExp'
+    type = 'Lin'
+
+    if type == 'LinExp':
+
+        peaks = [0.1,0.5]
+        maxs = np.ones((d,))*1
+        prm_add = [[m*1./peak*np.exp(1), 1./peak] for peak, m in zip(peaks,maxs)]
+        prm_lin = 1./0.02 # linear parameter
+        prm_lik = .01 # parameter of the likelihood
+
+        mod_ = LinExpAdditiveModel(prm_add,prm_lin,prm_lik)
+        mod = LinExpAdditiveModel(prm_add,prm_lin,prm_lik)
+
+    elif type == 'Lin':
+        prm_add = [[1],[.5]]
+        mod_ = LinAdditiveModel(prm_add,prm_lin,prm_lik)
+        mod = LinAdditiveModel(prm_add,prm_lin,prm_lik)
+
+    w = mod_.flat_prm(prm_add,prm_lin,prm_lik)
+
 
     # sampling
     x= 2*np.random.rand(n,d+1)-1
 
     x[:,0]*=.1
     x[:,1:-1]/=2.
-    b = mod.bern(x)
+    b = mod_.bern(x)
     y = (np.random.rand(n)<b).astype(int)
+
+    # learning
+    w0 = w
+    res = ml_fit(x,y,w0,mod,prop_bstrap=0.9,n_rep=10)
+
+    #----------------------------------------------------------------------------
+
 
     plt.figure()
     plt.hist(b)
     plt.savefig('ml-fit0.svg')
     plt.close()
-
-    # learning
-    res = []
-    n_rep = 40
-    prop_bstrap = .9
-    n_bstrap = int(prop_bstrap*n)
-    for i in range(n_rep):
-        if i%10==0: print i,
-        I = np.random.permutation(n)[:n_bstrap]
-        x_ = x[I,:]
-        y_ = y[I]
-        w0 = w
-        err = lambda w : -llh(w,x_,y_)
-        r = minimize(err,w0)
-        res.append(r.x)
-
-    print 'done'
-    res = np.vstack(res)
-
-    #-----------------
 
 
     fig,axarr = plt.subplots(1,d+1,figsize=[10,5])
@@ -118,15 +97,15 @@ if __name__ == '__main__':
 
 
     for i_r,r in enumerate(res):
-        p_add,_,_ = unflat_param(r)
+        p_add,_,_ = mod.unflat_param(r)
         for lag in range(d):
             kwargs = {'label':'lag'+str(lag+1)} if i_r==0 else {}
-            ax.plot(xp,mod.bias(p_add[lag][0],prm_add[lag][1])(xp),col[lag],**kwargs)
+            ax.plot(xp,mod.bias(p_add[lag])(xp),col[lag],**kwargs)
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles, labels)
 
     for lag,p in zip(range(d),prm_add):
-        ax.plot(xp,mod.bias(p[0],p[1])(xp),col[lag],\
+        ax.plot(xp,mod.bias(p)(xp),col[lag],\
                 linestyle='--',\
                linewidth=2,path_effects=[path_effects.Normal(),\
                                          path_effects.SimpleLineShadow()])
