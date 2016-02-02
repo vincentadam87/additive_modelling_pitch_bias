58f9d428ca4ad8b0b110c351891461316bb3d2dc
Dell - Linx <itay.lieder@mail.huji.ac.il>
2016-02-02 16:40:02 +0200

    added bimodal markov distibution

--- stimulus_generation/generate_stimuli.py
+++ stimulus_generation/generate_stimuli.py
@@ -63,48 +63,91 @@ def sample_s1_s2_bimodal(samples,franges,dfrange):
             s1[ii],s2[ii] = s2[ii],s1[ii]
     return s1,s2
 
+def sample_s1_s2_threshold(samples,frange,dfrange,threshold,condition):
+    '''
+    Sampling frequency pairs, first one according to uniform
+    Second one conditional on first setting a difficulty sampled from  U[-df,df]
+    with resampling if out of range bounds
+    :param samples: number of trials
+    :param frange: frequency range (list)
+    :param dfrange: difficulty range, in Hz
+    :return: s1, s2
+    '''
+    dfrange = np.log(dfrange)
+    s1 = np.random.rand(samples) * (np.log(frange[1]) - np.log(frange[0])) + np.log(frange[0])
+    s2 = np.empty(samples)
 
-#================================================================
-# OLD BELOW
-#================================================================
+    s2[0] = s1[0] + (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+    if np.random.rand()>0.5:
+        s2[0] = s1[0] - (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+    if np.random.rand()>0.5:
+        s1[0],s2[0] = s2[0],s1[0]
 
-'''
+    for ii in range(1,samples):
+        dist = s1[ii] - 0.5*(s1[ii-1]+s2[ii-1])
+        direction = np.sign(dist)
 
-def real_participant(p=11,width=1):
-    if width=='narrow':
-        mat= sio.loadmat('hillaTrainNarrow_1block.mat')
-        a,b = 940, 1060
-    elif width=='mid':
-        mat= sio.loadmat('hillaTrainMedium_1block.mat')
-        a,b = 800,1250
-    elif width=='broad':
-        mat= sio.loadmat('hillaTrainWide_1block.mat')
-        a,b = 670, 1500
-        
-    s1=mat['s1']
-    s2=mat['s2']
-    acc=mat['acc']
-    resp=mat['resp']
-    
-    return np.concatenate(s1[0][p]), \
-           np.concatenate(s2[0][p]), \
-           np.concatenate(resp[0][p]), \
-           a ,\
-           b, \
-#    acc=np.concatenate(acc[0][ii])
-#    resp=np.concatenate(resp[0][ii])
-          
-def load_bimodal_single_subject(i_s):
-    """
-    Loading (tone1,tone2, response) of Mturk bimodal experiment
-    :param i_s: index of subject
-    :return:
-    """
-    assert i_s in range(70), 'subject index must lie between 0 and 69'
-    data_path = './data/'
-    fname = 'BiModalTurk2.mat'
-    m = sio.loadmat(data_path+fname)
-    return m['s1'][:,i_s],m['s2'][:,i_s],m['acc'][:,i_s]
+        if condition == 'inner_minus':
+            if (np.abs(dist) < threshold):
+                s2[ii] = s1[ii] + direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+            else:
+                s2[ii] = s1[ii] - direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+        if condition == 'inner_plus':
+            if (np.abs(dist) < threshold):
+                s2[ii] = s1[ii] - direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+            else:
+                s2[ii] = s1[ii] + direction*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+    return s1,s2
+
+def sample_s1_s2_markov(samples, frange, dfrange, mrange_close, mrange_far, condition):
+    '''
+    Sampling frequency pairs, first one according to uniform
+    Second one conditional on first setting a difficulty sampled from  U[-df,df]
+    with resampling if out of range bounds
+    :param samples: number of trials
+    :param frange: frequency range (list)
+    :param dfrange: difficulty range, in Hz
+    :return: s1, s2
+    '''
+
+    if condition == 'inner_plus':
+        inner = -1
+    elif condition == 'inner_minus':
+        inner = 1
+
+    dfrange = np.log(dfrange)
+    s1 = np.empty(samples)
+    s2 = np.empty(samples)
+
+    s1[0] = np.random.rand() * (np.log(frange[1]) - np.log(frange[0])) + np.log(frange[0])
+    s2[0] = s1[0] + (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+    if np.random.rand()>0.5:
+        s2[0] = s1[0] - (np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+    if np.random.rand()>0.5:
+        s1[0],s2[0] = s2[0],s1[0]
+
+    for ii in range(1,samples):
+
+        if np.random.rand()>0.5:
+            # sample close
+            bias = 1
+            add_sub = np.random.randint(2)*2-1
+            s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_close[1] - mrange_close[0])+ mrange_close[0])
+            while (s1[ii] < np.log(frange[0])) or (s1[ii] > np.log(frange[1])):
+                add_sub = np.random.randint(2)*2-1
+                s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_close[1] - mrange_close[0])+mrange_close[0])
 
 
-'''
+        else:
+            # sample far
+            bias = -1
+            add_sub = np.random.randint(2)*2-1
+            s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_far[1] - mrange_far[0]) + mrange_far[0])
+            while (s1[ii] < np.log(frange[0])) or (s1[ii] > np.log(frange[1])):
+                add_sub = np.random.randint(2)*2-1
+                s1[ii] = 0.5*(s1[ii-1]+s2[ii-1]) + add_sub*(np.random.rand()*(mrange_far[1] - mrange_far[0]) + mrange_far[0])
+
+        dist = s1[ii] - 0.5*(s1[ii-1]+s2[ii-1])
+        s2[ii] = s1[ii] + inner*bias*np.sign(dist)*(np.random.rand()*(dfrange[1]-dfrange[0]) + dfrange[0])
+
+    return s1,s2
