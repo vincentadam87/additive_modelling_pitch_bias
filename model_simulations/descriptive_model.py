4b04d9057d1a257faba3419f2f98f8812c0914a1
vincentadam87 <vincent.adam87@gmail.com>
2016-02-02 20:16:09 +0200

    adding functions for ml fit

--- model_simulations/descriptive_model.py
+++ model_simulations/descriptive_model.py
@@ -3,6 +3,8 @@ from abc import abstractmethod
 from utils import phi
 from numpy import exp
 from matplotlib import pyplot as plt
+from scipy.optimize import minimize
+
 
 class AbstractModel(object):
     '''
@@ -62,6 +64,31 @@ class AdditiveModel(AbstractModel):
             alpha += a_(x[:,i_a+1])
         return self.lik(x[:,0]*self.p_lin-alpha)
 
+class LinAdditiveModel(AdditiveModel):
+    """
+    A descriptive model of the bias
+    Bias is deterministic, decision are noisy (probit model)
+    p(y=1|x0,x1,...,xtau) = phi( x0/p_lin - a(x1) - ... - a(xtau) )
+    where a :x = p_add[0]*x*exp(-|x|/p_add[1])
+    """
+
+    def __init__(self,prm_add=[],prm_lin=[],prm_lik=[]):
+        """ x : p[0]*x """
+        self.bias = lambda w : lambda x : w*x
+        self.lik = lambda l : lambda x : l/2+(1-l)*phi(x)
+        a = [self.bias(p) for p in prm_add]
+        super(LinAdditiveModel,self).__init__(a,prm_lin,self.lik(prm_lik))
+
+    def flat_prm(self,p_add,p_lin,p_lik):
+        return np.concatenate([np.array(p_add).flatten(),[p_lin],[p_lik]])
+
+    def unflat_param(self,w):
+        t = len(w)-2 # number of lags
+        p_add = [ [w[l]]  for l in range(t)]
+        p_lin = w[-2]
+        p_lik = w[-1]
+        return p_add,p_lin,p_lik
+
 class LinExpAdditiveModel(AdditiveModel):
     """
     A descriptive model of the bias
@@ -70,12 +97,53 @@ class LinExpAdditiveModel(AdditiveModel):
     where a :x = p_add[0]*x*exp(-|x|/p_add[1])
     """
 
-    def __init__(self,prm_add,prm_lin,prm_lik):
+    def __init__(self,prm_add=[],prm_lin=[],prm_lik=[]):
         """ x : p[0]*x*exp(-|x|/p[1]) """
-        self.bias = lambda w,d : lambda x : w*x*exp(-abs(x)*d)
+        self.bias = lambda w : lambda x : w[0]*x*exp(-abs(x)*w[1])
         self.lik = lambda l : lambda x : l/2+(1-l)*phi(x)
-        a = [self.bias(p[0],p[1]) for p in prm_add]
+        a = [self.bias(p) for p in prm_add]
         super(LinExpAdditiveModel,self).__init__(a,prm_lin,self.lik(prm_lik))
+
+    def flat_prm(self,p_add,p_lin,p_lik):
+        return np.concatenate([np.array(p_add).flatten(),[p_lin],[p_lik]])
+
+    def unflat_param(self,w):
+        t = len(w)/2-1 # number of lags
+        p_add = [[w[2*l],w[2*l+1]] for l in range(t)]
+        p_lin = w[-2]
+        p_lik = w[-1]
+        return p_add,p_lin,p_lik
+
+
+def ml_fit(x,y,w0,mod,prop_bstrap=0.9,n_rep=10):
+
+    def bern(w,x,mod):
+        p_add,p_lin,p_lik = mod.unflat_param(w)
+        mod.__init__(p_add,p_lin,p_lik)
+        b = mod.bern(x)
+        eps = 1e-5
+        b[b>1-eps]=1-eps
+        b[b<eps]=eps
+        return b
+
+    def llh(w,x,y,mod):
+        b = bern(w,x,mod)
+        return np.sum( (y*np.log(b) + (1-y)*np.log(1-b))  )
+
+    res = []
+    n = x.shape[0]
+    n_bstrap = int(prop_bstrap*n)
+    for i in range(n_rep):
+        if i%10==0: print i,
+        I = np.random.permutation(n)[:n_bstrap]
+        x_ = x[I,:]
+        y_ = y[I]
+        err = lambda w : -llh(w,x_,y_,mod)
+        r = minimize(err,w0)
+        res.append(r.x)
+    print 'done'
+    res = np.vstack(res)
+    return res
 
 
 
