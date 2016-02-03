bd8e643a914d76ab549f03a5a84396c209b2a068
vincentadam87 <vincent.adam87@gmail.com>
2016-02-03 22:52:07 +0200

    update scripts to test ml fitting

--- scripts/fit_wide_range_linear_part.py
+++ scripts/fit_wide_range_linear_part.py
@@ -8,9 +8,15 @@ import os
 
 n_rep = 30
 
-for model in ['lin','explin']:
-    for typ in ['good','poor']:
-        for thresh in [0.1, 0.5 , 1, 2]:
+for model in ['explin','lin']:
+
+    for thresh in [0.1,1]:
+
+        col = 'rgb'
+        fig1, axarr1 = plt.subplots(2,1)
+
+
+        for i_typ,typ in enumerate(['good','poor']):
 
             save_prefix = 'linear_part/'
             if not os.path.exists('./'+save_prefix):
@@ -41,6 +47,7 @@ for model in ['lin','explin']:
             x,y = get_trial_covariates(F1,F2,Y,T=T,inf=0)
             x = x.T
 
+
             #=========================================
             # Subselect close trials
             #=========================================
@@ -49,17 +56,18 @@ for model in ['lin','explin']:
             x =x[I,:]
             y =y[I]
             print x.shape, x[:,1].min(),x[:,1].max()
-
+            n_eff = x.shape[0]
             #=========================================
             # Declare model
             #=========================================
 
             if model == 'explin':
                 mod = LinExpAdditiveModel()
-                w0 = np.ones((2*T+2,))
+                w0 = np.zeros((2*T+2,))-5
+                #w0[:-3] = -5
             elif model == 'lin':
                 mod = LinAdditiveModel()
-                w0 = np.ones((T+2,))
+                w0 = np.zeros((T+2,))-5
 
             #=========================================
             # Run ML fit
@@ -77,7 +85,7 @@ for model in ['lin','explin']:
                 ax.scatter(res[:,2*lag],res[:,2*lag+1])
                 ax.set_xlabel('w')
                 ax.set_ylabel('decay')
-                ax.set_title('lag '+str(lag+1) )
+                ax.set_title('lag '+str(lag+1)+'/ n='+str(n_eff) )
             axarr[-1].scatter(res[:,-2],res[:,-1])
             axarr[-1].set_xlabel('lin')
             axarr[-1].set_ylabel('lik')
@@ -90,22 +98,21 @@ for model in ['lin','explin']:
 
             xmin, xmax = x[:,1].min(),x[:,1].max()
             xp = np.linspace(xmin,xmax,100)
-            col = 'rgb'
-            fig, ax = plt.subplots()
             #plt.hist(x[:,1:].flatten(),bins=100,alpha=0.2,normed=True)
 
 
             for i_r,r in enumerate(res):
-                p_add,_,_ = mod.unflat_param(r)
+                p_add,p_lin,_ = mod.unflat_param(r)
                 for lag in range(T):
-                    kwargs = {'label':'lag'+str(lag+1)} if i_r==0 else {}
-                    ax.plot(xp,mod.bias(p_add[lag])(xp),col[lag],**kwargs)
-            handles, labels = ax.get_legend_handles_labels()
-            ax.legend(handles, labels)
-
-            plt.savefig(save_prefix+'curves_'+save_name)
-
-            plt.close()
+                    kwargs = {'label':'lag'+str(lag+1)+'/ n='+str(n_eff)} if i_r==0 else {}
+                    axarr1[0].plot(xp,mod.bias(p_add[lag])(xp),col[i_typ],**kwargs)
+                    axarr1[1].plot(xp*p_lin,mod.bias(p_add[lag])(xp),col[i_typ],**kwargs)
+            handles, labels = axarr1[0].get_legend_handles_labels()
+            axarr1[0].legend(handles, labels)
 
 
-            plt.close('all')
+        plt.savefig(save_prefix+'curves_'+save_name)
+        plt.close()
+
+
+    plt.close('all')
--- scripts/ml_parametric_additve.py
+++ scripts/ml_parametric_additve.py
@@ -9,8 +9,7 @@ from matplotlib import pyplot as plt
 import matplotlib.patheffects as path_effects
 # ---------------------- Additive Linear Exp ---------------------
 
-
-
+isig = lambda x : -np.log(1./x-1)
 
 if __name__ == '__main__':
 
@@ -22,96 +21,99 @@ if __name__ == '__main__':
     d = 2
 
     prm_lin = 1./0.02 # linear parameter
-    prm_lik = .01 # parameter of the likelihood
+    prm_lik =  .01 # parameter of the likelihood
 
-    type = 'LinExp'
-    type = 'Lin'
-
-    if type == 'LinExp':
-
-        peaks = [0.1,0.5]
-        maxs = np.ones((d,))*1
-        prm_add = [[m*1./peak*np.exp(1), 1./peak] for peak, m in zip(peaks,maxs)]
-        prm_lin = 1./0.02 # linear parameter
-        prm_lik = .01 # parameter of the likelihood
-
-        mod_ = LinExpAdditiveModel(prm_add,prm_lin,prm_lik)
-        mod = LinExpAdditiveModel(prm_add,prm_lin,prm_lik)
-
-    elif type == 'Lin':
-        prm_add = [[1],[.5]]
-        mod_ = LinAdditiveModel(prm_add,prm_lin,prm_lik)
-        mod = LinAdditiveModel(prm_add,prm_lin,prm_lik)
-
-    w = mod_.flat_prm(prm_add,prm_lin,prm_lik)
+    for sim_type,fit_type in [['LinExp','LinExp'],['Lin','LinExp'],['LinExp','Lin'],['Lin','Lin']]:
 
 
-    # sampling
-    x= 2*np.random.rand(n,d+1)-1
+        if sim_type == 'LinExp':
+            peaks = [0.1,0.5]
+            maxs = [1,2]
+            prm_add = [[m*1./peak*np.exp(1), 1./peak] for peak, m in zip(peaks,maxs)]
+            mod_sim = LinExpAdditiveModel()
+            mod_sim.__init__(prm_add,prm_lin,prm_lik)
 
-    x[:,0]*=.1
-    x[:,1:-1]/=2.
-    b = mod_.bern(x)
-    y = (np.random.rand(n)<b).astype(int)
+        elif sim_type == 'Lin':
+            prm_add = [[2],[1]]
+            mod_sim = LinAdditiveModel()
+            mod_sim.__init__(prm_add,prm_lin,prm_lik)
 
-    # learning
-    w0 = w
-    res = ml_fit(x,y,w0,mod,prop_bstrap=0.9,n_rep=10)
+        if fit_type == 'Lin':
+            mod_fit = LinAdditiveModel()
+            w0 = np.zeros((d+2,))-5
+        elif fit_type == 'LinExp':
+            mod_fit = LinExpAdditiveModel()
+            w0 = np.zeros((2*d+2,))-5
 
-    #----------------------------------------------------------------------------
-
-
-    plt.figure()
-    plt.hist(b)
-    plt.savefig('ml-fit0.svg')
-    plt.close()
-
-
-    fig,axarr = plt.subplots(1,d+1,figsize=[10,5])
-    for lag,ax in zip(range(d),axarr.flat):
-        ax.scatter(res[:,2*lag],res[:,2*lag+1])
-        ax.plot(w[2*lag],w[2*lag+1],'xr')
-        #ax.set_xlim([0,2])
-        #ax.set_ylim([0,2])
-        ax.set_xlabel('w')
-        ax.set_ylabel('decay')
-        ax.set_title('lag '+str(lag+1) )
-    axarr[-1].scatter(res[:,-2],res[:,-1])
-    axarr[-1].plot(w[-2],w[-1],'xr')
-    #axarr[-1].set_xlim([0,2])
-    #axarr[-1].set_ylim([0,.3])
-    axarr[-1].set_xlabel('lin')
-    axarr[-1].set_ylabel('lik')
-    plt.tight_layout()
-    plt.savefig('ml-fit1.svg')
-    plt.close()
+        w = mod_sim.flat_prm(prm_add,prm_lin,prm_lik)
 
 
+        # sampling
+        x= (2*np.random.rand(n,d+1)-1)
+
+        x[:,0]*=.1
+        #x[:,1:-1]/=2.
+        b = mod_sim.bern(x)
+        y = (np.random.rand(n)<b).astype(int)
+
+        # learning
+        res = ml_fit(x,y,w0,mod_fit,prop_bstrap=0.9,n_rep=10,n_restart=5)
+
+        #----------------------------------------------------------------------------
 
 
-    xmin, xmax = x.min(),x.max()
-    xp = np.linspace(xmin,xmax,100)
-    col = 'rgb'
-    fig, ax = plt.subplots()
-    plt.hist(x[:,1:-1].flatten(),bins=100,alpha=0.2,normed=True)
+        plt.figure()
+        plt.hist(b)
+        plt.savefig('sim_'+sim_type+'_'+fit_type+'_ml-fit0.svg')
+        plt.close()
 
 
-    for i_r,r in enumerate(res):
-        p_add,_,_ = mod.unflat_param(r)
-        for lag in range(d):
-            kwargs = {'label':'lag'+str(lag+1)} if i_r==0 else {}
-            ax.plot(xp,mod.bias(p_add[lag])(xp),col[lag],**kwargs)
-    handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles, labels)
+        fig,axarr = plt.subplots(1,d+1,figsize=[10,5])
+        for lag,ax in zip(range(d),axarr.flat):
+            ax.scatter(res[:,2*lag],res[:,2*lag+1])
+            ax.plot(w[2*lag],w[2*lag+1],'xr')
+            #ax.set_xlim([0,2])
+            #ax.set_ylim([0,2])
+            ax.set_xlabel('w')
+            ax.set_ylabel('decay')
+            ax.set_title('lag '+str(lag+1) )
+        axarr[-1].scatter(res[:,-2],res[:,-1])
+        axarr[-1].plot(w[-2],w[-1],'xr')
+        #axarr[-1].set_xlim([0,2])
+        #axarr[-1].set_ylim([0,.3])
+        axarr[-1].set_xlabel('lin')
+        axarr[-1].set_ylabel('lik')
+        plt.tight_layout()
+        plt.savefig('sim_'+sim_type+'_'+fit_type+'_ml-fit1.svg')
+        plt.close()
 
-    for lag,p in zip(range(d),prm_add):
-        ax.plot(xp,mod.bias(p)(xp),col[lag],\
-                linestyle='--',\
-               linewidth=2,path_effects=[path_effects.Normal(),\
-                                         path_effects.SimpleLineShadow()])
-
-    plt.savefig('ml-fit2.svg')
-    plt.close()
 
 
-    plt.close('all')
+
+        xmin, xmax = x.min(),x.max()
+        xp = np.linspace(xmin,xmax,100)
+        col = 'rgb'
+        fig, ax = plt.subplots()
+        plt.hist(x[:,1:-1].flatten(),bins=100,alpha=0.2,normed=True)
+
+
+        for i_r,r in enumerate(res):
+            p_add,p_lin,p_lik = mod_fit.unflat_param(r)
+            for lag in range(d):
+                kwargs = {'label':'lag'+str(lag+1)} if i_r==0 else {}
+                mod_fit.set_params_uc(p_add,p_lin,p_lik)
+                ax.plot(xp,mod_fit.a[lag](xp),col[lag],**kwargs)
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(handles, labels)
+
+        for lag,p in zip(range(d),prm_add):
+            ax.plot(xp,mod_sim.a[lag](xp),col[lag],\
+                    linestyle='--',\
+                   linewidth=2,path_effects=[path_effects.Normal(),\
+                                             path_effects.SimpleLineShadow()])
+
+        plt.savefig('sim_'+sim_type+'_'+fit_type+'_ml-fit2.svg')
+        plt.close()
+
+
+        plt.close('all')
